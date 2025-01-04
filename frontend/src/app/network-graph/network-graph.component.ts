7211a76db65d1e83962961cb80b6fd0146ede821
Ksenia Nesytova <ksenia.nesytova@office.e2e4gu.ru>
2025-01-04 18:15:15 +0700

    add: graph component

---
+++ frontend/src/app/network-graph/network-graph.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-network-graph',
+  imports: [],
+  templateUrl: './network-graph.component.html',
+  styleUrl: './network-graph.component.scss'
+})
+export class NetworkGraphComponent {
+
+}
