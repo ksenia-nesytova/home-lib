5ae0239effab5d51b800a6d4d1b3204acec85c51
Ksenia Nesytova <ksenia.nesytova@office.e2e4gu.ru>
2025-01-04 18:14:38 +0700

    add: rputing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { NetworkGraphComponent } from './network-graph/network-graph.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'graph', component: NetworkGraphComponent },
+];
