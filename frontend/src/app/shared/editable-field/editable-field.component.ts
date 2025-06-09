24da1a728872fa7901d057ef9cbcbdc3e27f8ffc
Ksenia Nesytova <passedbyabyss@gmail.com>
2025-06-09 23:05:56 +0700

    add: editable-field component

---
+++ frontend/src/app/shared/editable-field/editable-field.component.ts
@@ -0,0 +1,52 @@
+import { Component, effect, input, InputSignal, output, signal, WritableSignal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-editable-field',
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './editable-field.component.html',
+  styleUrl: './editable-field.component.scss'
+})
+export class EditableFieldComponent {
+
+  public value: InputSignal<string> = input('');
+  public placeholder: InputSignal<string> = input('Click to edit');
+  public type = input<'text' | 'textarea'>('text');
+
+  public valueChange = output<string>();
+
+
+  protected isEditing: WritableSignal<boolean> = signal(false);
+  protected currentValue: WritableSignal<string> = signal('');
+
+
+  constructor() {
+    effect(() => {
+      if (!this.isEditing()) {
+        this.currentValue.set(this.value());
+      }
+    });
+  }
+
+
+  protected toggleEditMode(): void {
+    this.isEditing.set(true);
+    this.currentValue.set(this.value());
+  }
+
+  protected save(): void {
+    this.isEditing.set(false);
+    if (this.currentValue() !== this.value()) {
+      this.valueChange.emit(this.currentValue());
+    }
+  }
+
+  protected cancel(): void {
+    this.isEditing.set(false);
+    this.currentValue.set(this.value());
+  }
+
+
+}
