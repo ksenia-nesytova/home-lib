df4c07f8ba4978eded6b7566811c57ec17e3dcb0
Ksenia Nesytova <passedbyabyss@gmail.com>
2025-06-09 22:32:18 +0700

    add: model & enum for card

---
+++ frontend/src/app/shared/enums/media-type.ts
@@ -0,0 +1,10 @@
+export enum MediaType {
+    BOOK = 'Book',
+    PLAY = 'Play',
+    MOVIE = 'Movie',
+    MUSIC = 'Music',
+    TV_SHOW = 'TV Show',
+    VIDEO_GAME = 'Video Game',
+    PODCAST = 'Podcast',
+    OTHER = 'Other'
+};
---
+++ frontend/src/app/shared/models/Card.ts
@@ -0,0 +1,19 @@
+import { MediaType } from '../enums/media-type';
+
+export interface Card {
+    id: string;
+    coverImage?: string;
+    title: string;
+    author?: string;
+    mediaType: MediaType;
+    creationDate?: string; // or Date
+    language?: string;
+    tags: string[];
+    description?: string;
+    notes?: string;
+    location?: string;
+    rating?: number; // Number
+    dateAdded?: Date; // Date
+    isPhysicalCopy?: boolean;
+    downloadUrl?: string;
+}
