e253aeffee7bdfac894596d99286208afaadac7f
Ksenia Nesytova <passedbyabyss@gmail.com>
2025-06-11 22:07:59 +0700

    upd: shared styles

--- frontend/src/app/components/entity-card/entity-card.component.ts
+++ frontend/src/app/components/entity-card/entity-card.component.ts
@@ -2,16 +2,25 @@ import { Component, input, InputSignal } from '@angular/core';
 import { EditableFieldComponent } from '../../shared/editable-field/editable-field.component';
 import { Card } from '../../shared/models/Card';
 import { MediaType } from '../../shared/enums/media-type';
+import { FormsModule } from '@angular/forms';
+import { EditableSelectComponent } from '../../shared/editable-select/editable-select.component';
 
 @Component({
   selector: 'app-entity-card',
-  imports: [EditableFieldComponent],
+  imports: [
+    EditableFieldComponent,
+    FormsModule,
+    EditableSelectComponent
+  ],
   templateUrl: './entity-card.component.html',
   styleUrl: './entity-card.component.scss'
 })
 export class EntityCardComponent {
 
   protected readonly MEDIA_TYPES = MediaType;
+  protected readonly MEDIA_OPTIONS = Object.values(MediaType);
+  protected mediaOptionSelected = this.MEDIA_TYPES.PLAY;
+
 
   protected mockCardData: Card = {
     id: '1',
@@ -32,4 +41,14 @@ export class EntityCardComponent {
 
   protected card: InputSignal<Card> = input(this.mockCardData);
 
+  protected updateField(fieldName: string, newValue: string): void {
+    // this.card.set({ ...this.card, [fieldName]: newValue });
+
+    // Optionally: Save immediately to backend
+    // this.cardService.updateCardField(this.card.id, fieldName, newValue).subscribe();
+  }
+
+  protected onMediaTypeChange(mediaType: string) {
+    console.log(mediaType);
+  }
 }
