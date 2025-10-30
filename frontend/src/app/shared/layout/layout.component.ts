a2b0e90f87360038071bedf2431351482f2507fb
Ksenia Nesytova <choicechancecurse@gmail.com>
2025-10-30 13:47:38 +0700

    upd: divided layout into separate components

--- frontend/src/app/shared/layout/layout.component.ts
+++ frontend/src/app/shared/layout/layout.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '@app/core/layout/header/header.component';
+import { FooterComponent } from "@app/core/layout/footer/footer.component";
+import { SideNavComponent } from "@app/core/layout/side-nav/side-nav.component";
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, SideNavComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
