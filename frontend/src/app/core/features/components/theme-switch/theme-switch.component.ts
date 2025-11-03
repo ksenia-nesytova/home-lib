04854ad8672836f223c2d85a17b23844064d6bd6
Ksenia Nesytova <choicechancecurse@gmail.com>
2025-11-03 22:33:31 +0700

    add: theme-switch

---
+++ frontend/src/app/core/features/components/theme-switch/theme-switch.component.ts
@@ -0,0 +1,36 @@
+import { Component, effect, inject, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ThemeSwitchService } from '@app/services/theme-switch.service';
+import { HLTheme } from '@app/shared/enums/HLTheme';
+
+@Component({
+  selector: 'app-theme-switch',
+  imports: [
+    ReactiveFormsModule
+  ],
+  templateUrl: './theme-switch.component.html',
+  styleUrl: './theme-switch.component.scss'
+})
+export class ThemeSwitchComponent {
+  private themeService: ThemeSwitchService = inject(ThemeSwitchService);
+
+  protected isDarkMode = signal<boolean>(
+    this.themeService.currentTheme() === HLTheme.dark
+  );
+
+  public isActive = signal<boolean>(false);
+
+
+  protected active = new FormControl<boolean>(this.isActive(), { nonNullable: true });
+
+  constructor() {
+    effect(() => {
+      this.isDarkMode.set(this.themeService.currentTheme() === HLTheme.dark);
+    });
+  }
+
+  toggle() {
+    this.themeService.toggleTheme();
+  }
+}
+
