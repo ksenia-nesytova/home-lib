18a5008497068a80b41653a4ae1f11e698543235
Ksenia Nesytova <choicechancecurse@gmail.com>
2025-11-10 13:59:50 +0700

    upd: icon-list

--- frontend/src/assets/icons/navigation.ts
+++ frontend/src/assets/icons/navigation.ts
@@ -3,5 +3,6 @@ export const navigationIcons = {
     navigationArrowRight: `assets/icons/navigation/arrow-right.svg`,
     navigationCheck: `assets/icons/navigation/check.svg`,
     navigationClose: `assets/icons/navigation/close.svg`,
+    navigationExpand: `assets/icons/navigation/expand.svg`,
     navigationMenu: `assets/icons/navigation/menu.svg`,
 };
