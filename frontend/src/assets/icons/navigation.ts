d46bb1a895ed844eb0032c5742ce3707aba7352d
Ksenia Nesytova <choicechancecurse@gmail.com>
2025-11-10 13:43:55 +0700

    upd: minto style changes & add: icon

--- frontend/src/assets/icons/navigation.ts
+++ frontend/src/assets/icons/navigation.ts
@@ -1,6 +1,7 @@
 export const navigationIcons = {
     navigationArrowLeft: `assets/icons/navigation/arrow-left.svg`,
     navigationArrowRight: `assets/icons/navigation/arrow-right.svg`,
+    navigationCheck: `assets/icons/navigation/check.svg`,
     navigationClose: `assets/icons/navigation/close.svg`,
     navigationMenu: `assets/icons/navigation/menu.svg`,
 };
