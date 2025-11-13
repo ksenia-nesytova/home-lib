7a542153d0e009cefc453276ba58f055cad1b38e
Ksenia Nesytova <choicechancecurse@gmail.com>
2025-11-13 19:27:31 +0700

    upd: header

--- frontend/src/app/core/layout/header/header.component.ts
+++ frontend/src/app/core/layout/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { NavbarService } from '@app/services/navbar.service';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   imports: [
-    MatIconModule
+    MatIconModule,
+    RouterLink
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
