8f89783dd8abf9be7c2c0208862ce49fc7bcedd3
Ksenia Nesytova <choicechancecurse@gmail.com>
2025-12-17 11:54:20 +0700

    add: swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true
+  }));
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('HomeLib API')
+    .setDescription('API for HomeLib application')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
