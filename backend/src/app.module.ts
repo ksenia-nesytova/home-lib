82849ce4b3b358a7edf0823af882cde68fdb0671
Ksenia Nesytova <choicechancecurse@gmail.com>
2025-12-17 11:54:29 +0700

    upd: nest app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,8 +5,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { EntitiesModule } from './entities/entities.module';
 import { ConfigModule } from '@nestjs/config';
 
-
-
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -15,12 +13,12 @@ import { ConfigModule } from '@nestjs/config';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10),
+      port: +process.env.DB_PORT,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       entities: [],
-      synchronize: true,
+      autoLoadEntities: true,
     }),
     EntitiesModule,
   ],
